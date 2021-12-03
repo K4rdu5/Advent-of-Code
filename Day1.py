e24e1a67bbc664d551e24ed53024ae781d9c2fc5
Lucas <87875365+K4rdu5@users.noreply.github.com>
2021-12-03 16:47:27 +0100

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,34 @@
+#Day 1 part 1
+data = open("DataDay1.txt", "r").read().split("\n")
+sum = 1
+i = 0
+for height in data:
+    if height >= data[i-1]:
+        sum = sum + 1
+    i = i + 1
+print(sum)
+
+#Day 1 part 2
+data = open("DataDay1.txt", "r").read().split("\n")
+data3 = []
+i=0
+j=3
+
+for d in data:
+    if j == 2001:
+        continue
+    lst = data[i:j]
+    sum = int(lst[0]) + int(lst[1]) + int(lst[2])
+    data3.append(sum)
+    
+    i = i + 1
+    j = j + 1
+
+bigger = 0
+n = 0
+for height in data3:
+    print(height)
+    if height > data3[n-1]:
+        bigger = bigger + 1
+    n = n + 1
+print(bigger)
