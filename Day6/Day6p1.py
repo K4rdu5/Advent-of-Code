f0a094b99cc2bfd611affcc79d546ba5452ce370
Lucas <87875365+K4rdu5@users.noreply.github.com>
2021-12-06 13:07:59 +0100

    Create Day6p1.py

---
+++ Day6/Day6p1.py
@@ -0,0 +1,12 @@
+#Day 6 part 1
+
+datastring = open("DataDay6.txt", "r").read().split(",")
+data = [int(x) for x in datastring]
+
+for i in range(80):
+    for j in range(len(data)):
+        if data[j] == 0:
+            data[j] = 7
+            data.append(8)
+        data[j] = data[j]-1
+print(len(data))
