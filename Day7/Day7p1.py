1c8f28f8a18fa9801f0bf4d4a75571133dae21b6
Lucas <87875365+K4rdu5@users.noreply.github.com>
2021-12-07 18:16:19 +0100

    Update Day7p1.py

--- Day7/Day7p1.py
+++ Day7/Day7p1.py
@@ -5,14 +5,13 @@ import math
 data = open("DataDay7.txt", "r").read().split(",")
 crabs = [int(crab) for crab in data]
 
-#Get position
+#Get best position
 pos = statistics.median(crabs)
 
 #Calculate fuelcost
 fuelcost = []
 for d in crabs:
-    fuel = math.fabs(d-pos)
-    fuelcost.append(fuel)
+    fuelcost.append(math.fabs(d-pos))
 fuelcost_tot = sum(fuelcost)
 
 print(f"The cheapest alternative for the crabs is position {pos}, and cost {fuelcost_tot} fuel")
