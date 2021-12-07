541551d2be0137a83de5928ddd0510a5319f38f2
Lucas <87875365+K4rdu5@users.noreply.github.com>
2021-12-07 23:42:23 +0100

    Update Day2.py

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -6,13 +6,13 @@ depth = 0
 for d in data:
     if "forward" in d:
         datafor = d.split("forward ")
-        horizontal = horizontal + int(datafor[1])
+        horizontal += int(datafor[1])
     if "up" in d:
         dataup = d.split("up ")
-        depth = depth - int(dataup[1])
+        depth -= int(dataup[1])
     if "down" in d:
         datadown = d.split("down ")
-        depth = depth + int(datadown[1])
+        depth += int(datadown[1])
 
 print(horizontal * depth)
 
@@ -25,14 +25,18 @@ aim = 0
 for d in data:
     if "forward" in d:
         datafor = d.split("forward ")
-        horizontal = horizontal + int(datafor[1])
-        depth = depth + int(datafor[1]) * aim
+        horizontal += int(datafor[1])
+        depth += int(datafor[1]) * aim
         
     if "up" in d:
         dataup = d.split("up ")
-        aim = aim - int(dataup[1])
+        aim -= int(dataup[1])
         
     if "down" in d:
+        datadown = d.split("down ")
+        aim += int(datadown[1])
+        
+print(horizontal * depth)
         datadown = d.split("down ")
         aim = aim + int(datadown[1])
         
