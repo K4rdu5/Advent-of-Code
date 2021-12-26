d2c7dc1ae44e358d359d09ea9021d8395cf9d607
Lucas <87875365+K4rdu5@users.noreply.github.com>
2021-12-26 01:31:04 +0100

    Update Day8p1.py

--- Day8/Day8p1.py
+++ Day8/Day8p1.py
@@ -1,15 +1,22 @@
 #Day 8 part 1
+
+#easy
+#     "1" : 2
+#     "4" : 4
+#     "7" : 3
+#     "8" : 7
+
 with open("DataDay8.txt", "r") as f:
-    lines = f.read().split("\n")
+    lines = f.readlines()
 
 def difficulty(lst):
-    hard=[]
+    hard = []
     easy = []
     for line in lst:
         inp, outp = line.split("|")
         inp, outp = inp.split(), outp.split()
         for out in outp:
-            if len(out) == 2 or len(out) == 3 or len(out) == 4 or len(out) == 7:
+            if len(out) in [2,4,3,7]:
                 easy.append(out)
             else:
                 hard.append(out)
